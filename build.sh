7c3d1508ae6a57ee238f97f9683b799e80f466d3
Rasheed Ayoade <sn032945@firstbanknigeria.com>
2026-03-14 13:17:31 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Build Tailwind CSS
+python manage.py tailwind build
 
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run migrations
 python manage.py migrate
